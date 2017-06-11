069d08f4880927645fef50682dc61f501fc43bb3
Taki Vlasakakis <taki.vlasakakis@tapinfluence.com>
2017-06-10 20:02:33 -0600

    all fixed, back at 1:42 min

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,6 +1,22 @@
+/* eslint-disable */
 import React from 'react'
-import { render } from 'react-dom'
-import { BrowserRouter } from 'react-router'
-import App from './App'
+import ReactDOM from 'react-dom'
+import MyTitle from './MyTitle'
 
-render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('app'))
+var div = React.DOM.div
+
+var MyTitleFactory = React.createFactory(MyTitle) // convenience method{title: '
+
+var MyFirstComponent = React.createClass({
+  render: function () {
+    return (
+      div(null,
+        MyTitleFactory({title: 'props are da best', color: 'peru'}),
+        MyTitleFactory({title: 'semicolons are the worst', color: 'mediumaquamarine'}),
+        MyTitleFactory({title: 'really i hate semicolons', color: 'rebeccapurple'}),
+        MyTitleFactory({title: 'lololololol', color: 'tomato'})
+      )
+    )
+  }
+})
+ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))

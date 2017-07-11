850629b7ec80f8fe2d5688a058a0ca39bca732ca
Taki Vlasakakis <taki.vlasakakis@tapinfluence.com>
2017-07-11 13:12:27 -0600

    save

--- js/Header.js
+++ js/Header.js
@@ -1,31 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router'
-import { connect } from 'react-redux'
-import { setSearchTerm } from './actionCreators'
-const { func, bool, string } = React.PropTypes
+// import { connect } from 'react-redux'
+// import { setSearchTerm } from './actionCreators'
+// const { func, bool, string } = React.PropTypes
 
-const Header = React.createClass({
-  propTypes: {
-    showSearch: bool,
-    searchTerm: string,
-    dispatch: func
-  },
-  handleSearchTermChange (event) {
-    this.props.dispatch(setSearchTerm(event.target.value))
-  },
+class Header extends React.Component {
   render () {
-    let utilSpace
-    if (this.props.showSearch) {
-      utilSpace = <input type='text' placeholder='Search' value={this.props.searchTerm} onChange={this.handleSearchTermChange} />
-    } else {
-      utilSpace = (
-        <h2 className='header-back'>
-          <Link to='/search'>
-            Back
-          </Link>
-        </h2>
-      )
-    }
     return (
       <header>
         <h1>
@@ -33,16 +13,86 @@ const Header = React.createClass({
             svideo
           </Link>
         </h1>
-        {utilSpace}
       </header>
     )
   }
-})
-
-const mapStateToProps = (state) => {
-  return {
-    searchTerm: state.searchTerm
-  }
 }
 
-export default connect(mapStateToProps)(Header)
+const { func, bool, string } = React.PropTypes
+Header.propTypes = {
+  handleSearchTermChange: func,
+  showSearch: bool,
+  searchTerm: string
+}
+
+export default Header
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const Header = React.createClass({
+//   propTypes: {
+//     showSearch: bool,
+//     searchTerm: string,
+//     dispatch: func
+//   },
+//   handleSearchTermChange (event) {
+//     this.props.dispatch(setSearchTerm(event.target.value))
+//   },
+//   render () {
+//     let utilSpace
+//     if (this.props.showSearch) {
+//       utilSpace = <input type='text' placeholder='Search' value={this.props.searchTerm} onChange={this.handleSearchTermChange} />
+//     } else {
+//       utilSpace = (
+//         <h2 className='header-back'>
+//           <Link to='/search'>
+//             Back
+//           </Link>
+//         </h2>
+//       )
+//     }
+//     return (
+//       <header>
+//         <h1>
+//           <Link to='/'>
+//             svideo
+//           </Link>
+//         </h1>
+//         {utilSpace}
+//       </header>
+//     )
+//   }
+// })
+
+// const mapStateToProps = (state) => {
+//   return {
+//     searchTerm: state.searchTerm
+//   }
+// }
+
+// export default connect(mapStateToProps)(Header)

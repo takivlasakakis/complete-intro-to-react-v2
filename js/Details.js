96f7a75f7a678a4d066e30b325d6b33e607b0bca
Taki Vlasakakis <takivlasakakis@Takis-MacBook-Pro.local>
2017-08-03 11:17:56 -0600

    @7:33

--- js/Details.js
+++ js/Details.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './Header'
+import axios from 'axios'
 const { shape, string } = React.PropTypes
 
 const Details = React.createClass({
@@ -9,21 +10,41 @@ const Details = React.createClass({
       year: string,
       poster: string,
       trailer: string,
-      description: string
+      description: string,
+      imdbID: string
     })
+  },
+  getInitialState () {
+    return {
+      omdbData: {}
+    }
+  },
+  componentDidMount () {
+    axios.get(`http://www.omdbapi.com/?i=${this.props.show.imdbID}`)
+    .then((response) => {
+      this.setState({omdbData: response.data})
+    })
+    .catch((error) => console.error('acios error', error))
   },
   render () {
     const { title, description, year, poster, trailer } = this.props.show
+    let rating
+    if (this.state.omdbData.imdbRating) {
+      rating = <h3>{this.state.omdbData.imdbRating}</h3>
+    } else {
+      rating = <img src='/public/img/loading.png' alt='loading indicator' />
+    }
     return (
       <div className='details'>
         <Header />
         <section>
           <h1>{title}</h1>
           <h2>({year})</h2>
+          {rating}
           <img src={`/public/img/posters/${poster}`}/>
           <p>{description}</p>
         </section>
-        <div>
+      <div>
           <iframe src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
           frameBorder='0' allowFullScreen />
         </div>
